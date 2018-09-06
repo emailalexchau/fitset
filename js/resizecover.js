8883c0f5e36c98fca64d691a94610c23e6c49e49
Alex Chau <emailalexchau@gmail.com>
2018-09-06 12:31:21 -0600

    Fix cover image spacing

--- js/resizecover.js
+++ js/resizecover.js
@@ -1,12 +1,20 @@
 $(document).ready(function(){
 	'use strict';
-	if ($(window).height() > 750) {
-		$('#top').css('height', $(window).height() - 300);
+	if ($(window).height() > 750 && $(window).height() <= 1000) {
+		$('#top').css('height', $(window).height() - 350);
+	} else if ($(window).height() > 1000) {
+		$('#top').css('height', 700);
+	} else {
+		$('#top').css('height', 400);
 	}
 });
 $(window).resize(function(){
 	'use strict';
-	if ($(window).height() > 750) {
-		$('#top').css('height', $(window).height() - 300);
+	if ($(window).height() > 750 && $(window).height() <= 1000) {
+		$('#top').css('height', $(window).height() - 350);
+	} else if ($(window).height() > 1000) {
+		$('#top').css('height', 700);
+	} else {
+		$('#top').css('height', 400);
 	}
 });
