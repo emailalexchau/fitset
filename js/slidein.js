43ab661d7c6aa71a8d35358c8ee9c342a8daaf5c
Alex Chau <emailalexchau@gmail.com>
2018-09-06 21:11:01 -0600

    Add animation

---
+++ js/slidein.js
@@ -0,0 +1,26 @@
+var $animation_elements = $('.section');
+var $window = $(window);
+
+function check_if_in_view() {
+	'use strict';
+	var window_height = $window.height();
+	var window_top_position = $window.scrollTop();
+	var window_bottom_position = (window_top_position + window_height);
+
+	$.each($animation_elements, function () {
+		var $element = $(this);
+		var element_height = $element.outerHeight();
+		var element_top_position = $element.offset().top;
+		var element_bottom_position = (element_top_position + element_height);
+
+		if ((element_bottom_position >= window_top_position) &&
+			(element_top_position <= window_bottom_position)) {
+			$element.addClass('scale');
+		} else {
+			// $element.removeClass('scale');
+		}
+	});
+}
+
+$window.on('scroll resize', check_if_in_view);
+$window.trigger('scroll');
