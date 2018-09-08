83ac59700c6d9319f0e6f3ce49c4bae55e0158c7
Alex Chau <emailalexchau@gmail.com>
2018-09-08 00:57:00 -0600

    Fix mobile menu

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,5 @@
 // Is Menu in Mobile View?
-$(document).ready(function(){
+$(document).ready(function () {
 	'use strict';
 	if ($(window).width() <= 800) {
 		$('#menu').addClass('mobile');
@@ -7,12 +7,11 @@ $(document).ready(function(){
 		$('#menu').removeClass('mobile');
 	}
 });
-$(window).resize(function(){
+$(window).resize(function () {
 	'use strict';
 	if ($(window).width() <= 800) {
 		$('#menu').addClass('mobile');
 		if (sessionStorage.getItem('menuIsVisible') === 'true') {
-			$('#menu').slideDown(300);
 			$('#hamburger').addClass('close');
 		} else {
 			$('#menu').hide();
@@ -23,26 +22,28 @@ $(window).resize(function(){
 		$('#menu').show();
 		$('body').css('overflow', 'scroll');
 		$('body').css('position', 'static');
+		sessionStorage.setItem('menuIsVisible', 'false');
 	}
 });
 
-$('#hamburger').click(function() {
+$('#hamburger').click(function () {
 	'use strict';
 	if (sessionStorage.getItem('menuIsVisible') === 'true') {
-		sessionStorage.setItem('menuIsVisible', 'false');
 		$('#menu').slideUp(300);
 		$('#hamburger').removeClass('close');
+		sessionStorage.setItem('menuIsVisible', 'false');
+		// Lock Screen Position
 		$('body').css('overflow', 'scroll');
 		$('body').css('position', 'static');
-		$('.quotes').css('display','block');
-		
+		$('.quotes').css('display', 'block');
 	} else {
-		sessionStorage.setItem('menuIsVisible', 'true');
 		$('#menu').slideDown(300);
 		$('#hamburger').addClass('close');
+		sessionStorage.setItem('menuIsVisible', 'true');
+		// Unlock Screen Position
 		$('body').css('overflow', 'hidden');
 		$('body').css('position', 'fixed');
-		$('.quotes').css('display','none');
+		$('.quotes').css('display', 'none');
+
 	}
 });
-
