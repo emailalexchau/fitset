f1546a54cc251e1c9595f0beeca788a5b7309409
Alex Chau <emailalexchau@gmail.com>
2018-09-10 15:23:22 -0600

    Add sticky menu

--- js/menu.js
+++ js/menu.js
@@ -1,49 +1,86 @@
-// Is Menu in Mobile View?
 $(document).ready(function () {
 	'use strict';
+	var scroll = 0;
+	var menuIsVisible = null;
+	
 	if ($(window).width() <= 800) {
 		$('#menu').addClass('mobile');
 	} else {
 		$('#menu').removeClass('mobile');
 	}
-});
-$(window).resize(function () {
-	'use strict';
-	if ($(window).width() <= 800) {
-		$('#menu').addClass('mobile');
-		if (sessionStorage.getItem('menuIsVisible') === 'true') {
-			$('#hamburger').addClass('close');
+	
+	$(window).resize(function () {
+		if ($(window).width() <= 800) {
+			$('#menu').addClass('mobile');
+			if (menuIsVisible === 'true') {
+				$('#hamburger').addClass('close');
+			} else {
+				$('#menu').hide();
+				$('#hamburger').removeClass('close');
+			}
 		} else {
-			$('#menu').hide();
-			$('#hamburger').removeClass('close');
+			$('#menu').removeClass('mobile');
+			$('#menu').show();
+			$('body').css('overflow-y', 'scroll');
+			$('body').css('position', 'static');
+			if (scroll === 0) {
+				$("#top").removeClass("opaque");
+			}
+			$('.quotes').css('display', 'block');
+			menuIsVisible = false;
 		}
-	} else {
-		$('#menu').removeClass('mobile');
-		$('#menu').show();
-		$('body').css('overflow', 'scroll');
-		$('body').css('position', 'static');
-		sessionStorage.setItem('menuIsVisible', 'false');
-	}
+	});
+	
+	$('#hamburger').click(function () {
+		if (menuIsVisible === true) {
+			$('#menu').slideUp(300);
+			$('#hamburger').removeClass('close');
+			menuIsVisible = false;
+			// Unlock Screen Position
+			$('body').css('overflow-y', 'scroll');
+			$('body').css('position', 'static');
+			$('.quotes').css('display', 'block');
+			scroll = window.scrollTo(0, scroll);
+			
+			if (scroll < 70) {
+				$("#top").addClass("opaque");
+			}
+		} else {
+			scroll = $(window).scrollTop();
+			$('#menu').slideDown(300);
+			$('#hamburger').addClass('close');
+			menuIsVisible = true;
+			// Lock Screen Position
+			$('body').css('overflow-y', 'hidden');
+			$('body').css('position', 'fixed');
+			$('.quotes').css('display', 'none');
+			// Scroll Window Back to Original Position
+			
+		}
+	});
+	
+	// Add Dark Background to Menu on Scroll
+	$(window).scroll(function () {
+		scroll = $(window).scrollTop();
+
+		if (scroll >= 70) {
+			$("#top").addClass("opaque");
+		} else {
+			$("#top").removeClass("opaque");
+		}
+
+		if (menuIsVisible === true) {
+			$("#top").addClass("opaque");
+		}
+
+	});
+
 });
 
-$('#hamburger').click(function () {
-	'use strict';
-	if (sessionStorage.getItem('menuIsVisible') === 'true') {
-		$('#menu').slideUp(300);
-		$('#hamburger').removeClass('close');
-		sessionStorage.setItem('menuIsVisible', 'false');
-		// Lock Screen Position
-		$('body').css('overflow', 'scroll');
-		$('body').css('position', 'static');
-		$('.quotes').css('display', 'block');
-	} else {
-		$('#menu').slideDown(300);
-		$('#hamburger').addClass('close');
-		sessionStorage.setItem('menuIsVisible', 'true');
-		// Unlock Screen Position
-		$('body').css('overflow', 'hidden');
-		$('body').css('position', 'fixed');
-		$('.quotes').css('display', 'none');
 
-	}
-});
+
+
+
+
+
+
