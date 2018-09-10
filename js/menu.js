3cff9e2f61fc5b2ffc670f626958d8aaeb04fa6b
Alex Chau <emailalexchau@gmail.com>
2018-09-10 16:15:34 -0600

    Revert to absolute top bar

--- js/menu.js
+++ js/menu.js
@@ -9,6 +9,45 @@ $(document).ready(function () {
 		$('#menu').removeClass('mobile');
 	}
 	
+	// Add Dark Background to Menu on Scroll
+	$(window).scroll(function () {
+		var y = $(window).scrollTop();
+
+		if (y >= 70) {
+			$("#top").addClass("opaque");
+		} else {
+			$("#top").removeClass("opaque");
+		}
+		if (menuIsVisible === true) {
+			$("#top").addClass("opaque");
+		}
+	});
+	
+	$('#hamburger').click(function () {
+		
+		if (menuIsVisible === true) {
+			menuIsVisible = false;
+			$('#menu').hide();
+			$('#hamburger').removeClass('close');
+			// Unlock Screen Position
+			$('body').css('overflow', 'scroll');
+			$('body').css('position', 'static');
+			$('.quotes').css('display', 'block');
+			scroll = window.scrollTo(0, scroll);
+		} else {
+			scroll = $(window).scrollTop();
+			// Lock Screen Position
+			$('body').css('overflow', 'hidden');
+			$('body').css('position', 'fixed');
+			$('.quotes').css('display', 'none');
+			$('#hamburger').addClass('close');
+			
+			$('#menu').show();
+			
+			menuIsVisible = true;
+		}
+	});
+	
 	$(window).resize(function () {
 		if ($(window).width() <= 800) {
 			$('#menu').addClass('mobile');
@@ -30,39 +69,7 @@ $(document).ready(function () {
 			menuIsVisible = false;
 		}
 	});
-	
-	$('#hamburger').click(function () {
-		if (menuIsVisible === true) {
-			$('#hamburger').removeClass('close');
-			// Unlock Screen Position
-			$('body').css('overflow', 'scroll');
-			$('body').css('position', 'static');
-			$('.quotes').css('display', 'block');
-			scroll = window.scrollTo(0, scroll);
-			menuIsVisible = false;
-			$('#menu').hide();
-		} else {
-			scroll = $(window).scrollTop();
-			// Lock Screen Position
-			$('body').css('overflow', 'hidden');
-			$('body').css('position', 'fixed');
-			$('.quotes').css('display', 'none');
-			$('#hamburger').addClass('close');
-			menuIsVisible = true;
-			$('#menu').show();
-		}
-	});
-	
-	// Add Dark Background to Menu on Scroll
-	$(window).scroll(function () {
-		var y = $(window).scrollTop();
 
-		if (y >= 70) {
-			$("#top").addClass("opaque");
-		} else {
-			$("#top").removeClass("opaque");
-		}
-	});
 });
 
 
